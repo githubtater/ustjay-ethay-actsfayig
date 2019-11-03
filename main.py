4c55594bd113aee6cbbd709a4946447f90692d5d
Roy Tate <roytate@hotmail.com>
2019-11-02 22:34:42 -0700

    now printing fact and url on different lines

--- main.py
+++ main.py
@@ -6,32 +6,8 @@ from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
-# < html >
-# < head >
-# < title > Pig
-# Latin < / title >
-# < / head >
-# < body >
-# < h1 > Pig
-# Latin < / h1 >
-# < h2 > Let
-# them
-# eat
-# pig
-# latin! < / h2 >
-#
-# < form
-# method = "POST"
-# action = "/piglatinize/" >
-# < textarea
-# name = "input_text" > < / textarea >
-# < input
-# type = "submit"
-# value = "Submit" >
-# < / form >
-#
-# < / body >
-# < / html >
+fact_string = 'The random fact is of the day is:<b>  {}</b><br>' \
+        'Click here to view it in Pig Latin: {}'
 
 
 def get_fact():
@@ -43,8 +19,6 @@ def get_fact():
 
 @app.route('/')
 def home():
-    # response = requests.get("https://hidden-journey-62459.herokuapp.com/")
-    # soup = BeautifulSoup(response.content, "html.parser")
     latin_url = "https://hidden-journey-62459.herokuapp.com/piglatinize/"
     fact = get_fact().strip()
     payload = {'input_text': fact}
@@ -52,7 +26,7 @@ def home():
     soup = BeautifulSoup(r.content, "html.parser")
     url = soup.find('a', href=True)
     build_url = latin_url.replace('/piglatinize/', '') + url.text
-    return f'<a href="{build_url}">{build_url}</a>'
+    return fact_string.format(fact, f'<a href="{build_url}">{build_url}</a>')
 
 
 if __name__ == "__main__":
